5979dd3eb504d5af3e66ec3559b99e8c3deaec7e
Marian Domanski <mmdski@gmail.com>
2025-04-11 18:32:10 -0500

    make nf shorter and set bf to default

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -3,9 +3,9 @@
 (defvar md/small-frame-alist
   '((width . 85) (height . 25) (left . 10) (top . 30)))
 (defvar md/narrow-frame-alist
-  '((width . 85) (height . 46) (left . 10) (top . 30)))
+  '((width . 85) (height . 35) (left . 10) (top . 30)))
 
-(setq default-frame-alist md/small-frame-alist)
+(setq default-frame-alist md/big-frame-alist)
 
 (add-to-list 'default-frame-alist '(z-group . above))
 
